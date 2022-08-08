b6a8accdb86a9f11084d728b946057ac1e9db02b
HelloHailie <alsltjsals@gmail.com>
2022-08-09 00:41:32 +0900

    finding error

--- section28/src/components/Ingredients/Ingredients.js
+++ section28/src/components/Ingredients/Ingredients.js
@@ -1,36 +1,16 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
-import Search from "./Search";
 import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  //렌더링이 두번 된다. 왜냐면 Search 컴포넌트에서도 가지고 오기 때문이다.
-  // useEffect(() => {
-  //   fetch(
-  //     "https://react-hooks-update-with-max-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((responseData) => {
-  //       const loadedIngredients = [];
-  //       for (const key in responseData) {
-  //         loadedIngredients.push({
-  //           id: key,
-  //           title: responseData[key].title,
-  //           amount: responseData[key].amount,
-  //         });
-  //       }
-  //       setUserIngredients(loadedIngredients);
-  //     });
-  // }, []);
-
   useEffect(() => {
-    console.log("rendering", userIngredients);
+    console.log("RENDERING INGREDIENTS", userIngredients);
   }, [userIngredients]);
 
-  //callback hook을 사용했기 때문에 Ingredients 컴포넌트가 리렌더링되도,이 함수는 새로 생성되지 않았기 때문에 값이 변하지 않는다.
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
   }, []);
@@ -51,19 +31,26 @@ const Ingredients = () => {
         setUserIngredients((prevIngredients) => [
           ...prevIngredients,
           { id: responseData.name, ...ingredient },
-          //{ id: Math.random().toString(), ...ingredient },
         ]);
       });
   };
 
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className='App'>
-      <IngredientForm onAddIngredientHandler={addIngredientHandler} />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
-        {/* Need to add list here! */}
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
--- section28/src/components/Ingredients/Search.js
+++ section28/src/components/Ingredients/Search.js
@@ -1,34 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo((props) => {
-  const { onLoadIngredients } = props;
+  const { onLoadIngredients } = props; //onLoadIngredients함수가 바뀔 때마다 렌더링하고 싶다. 객체 구조분해할당으로 써줌
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(
-      "https://react-hooks-update-with-max-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json" +
-        query
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    //firebase는 필러링을 지원한다.
+
+    //입력된 내용이 타이머가 시작된 시점에 입력된 내용과 같으면 사용자가 입력을 멈췄다는 뜻이니까 그때만 요청을 보낸다.
+    const timer = setTimeout(() => {
+      // 0.5초 후 확인 하는 코드
+      //타이머가 설정된 때 유저가 입력한 값 === 유저가 현재 입력한 코드
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://react-hooks-update-with-max-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json" +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            // Ingredients 컴포넌트에서 불러온다. 여기서 관리하니까
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+    //useEffect는 함수를 반환할 수 있다.(clean up 함수)
+    //동일한 useEffect함수가 다시 실행되기 전에 실행된다.
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className='search'>
@@ -36,6 +52,7 @@ const Search = React.memo((props) => {
         <div className='search-input'>
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type='text'
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
